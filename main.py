5f2d6a2561bbc290e7615af8d8f0e7a5a67355ba
Dryg1214 <pakazancev901@gmail.com>
2021-12-09 02:03:53 +0400

    Удален хлам и сделан autopep8

--- main.py
+++ main.py
@@ -8,20 +8,14 @@ from cryptography.hazmat.primitives import padding as padding2
 from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.serialization import load_pem_public_key, load_pem_private_key
 
-#Входные параметры:
-# 1) путь, по которому сериализовать зашифрованный симметричный ключ;
-# 2) путь, по которому сериализовать открытый ключ;
-# 3) путь, по которому сериазизовать закрытый ключ
-# 1.1. Сгеренировать ключ для симметричного алгоритма.
-# 1.2. Сгенерировать ключи для ассиметричного алгоритма.
-# 1.3. Сериализовать ассиметричные ключи.
-# 1.4. Зашифровать ключ симметричного шифрования открытым ключом и сохранить по указанному пути.
+
 def generate_sym_key():
     """Генерация симметричного ключа"""
     key = os.urandom(16)
     print("Симетричный ключ сгенерирован")
     print(key)
     return key
+
 
 def generate_assym_key():
     """Генерация ассиметричных ключей"""
@@ -31,19 +25,21 @@ def generate_assym_key():
     )
     return keys
 
+
 def serialize_assym_keys(as_key):
     """Сериализация ассиметричных ключей"""
     path_open = input("Введите путь для открытого ключа ")
     with open(path_open, 'wb') as public_out:
         public_out.write(as_key.public_key().public_bytes(encoding=serialization.Encoding.PEM,
-                                                 format=serialization.PublicFormat.SubjectPublicKeyInfo))
+                                                          format=serialization.PublicFormat.SubjectPublicKeyInfo))
     print("Ассиметричный открытый ключ записан в файл")
     path_close = input("Введите путь для закрытого ключа ")
     with open(path_close, 'wb') as private_out:
         private_out.write(as_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                                    format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                    encryption_algorithm=serialization.NoEncryption()))
+                                               format=serialization.PrivateFormat.TraditionalOpenSSL,
+                                               encryption_algorithm=serialization.NoEncryption()))
     print("Ассиметричный закрытый ключ записан в файл")
+
 
 def sym_key_encrypt(sym_key):
     """Шифрование симметричного ключа открытым ключом и запись его в файл"""
@@ -60,10 +56,11 @@ def sym_key_encrypt(sym_key):
             label=None
         )
     )
-    path_enc = input("Введите путь для зашифрованного симметричного ключа")
+    path_enc = input("Введите путь для зашифрованного симметричного ключа ")
     with open(path_enc, "wb") as f:
         f.write(ciphertext)
     print("Зашифрованный симметричный ключ записан в файл")
+
 
 def hybrid_system_key_generate_without():
     """Генерация ключей гибридной системы"""
@@ -72,59 +69,12 @@ def hybrid_system_key_generate_without():
     serialize_assym_keys(ass_key)
     sym_key_encrypt(key)
 
-def hybrid_system_key_generate(path_enc, path_open, path_close):
-    # Генерация ключа для симметричного алгоритма.
-    key = os.urandom(16)
-    print("Симетричный ключ сгенерирован")
-    # Генерация ключей для ассиметричного алгоритма.
-    keys = rsa.generate_private_key(
-        public_exponent=65537,
-        key_size=2048
-    )
-    private_key = keys
-    public_key = keys.public_key()
-    print("Ассиметричный ключи сгенерирован")
-    # сериализация открытого ключа в файл
-    with open(path_open, 'wb') as public_out:
-        public_out.write(public_key.public_bytes(encoding=serialization.Encoding.PEM,
-                                                 format=serialization.PublicFormat.SubjectPublicKeyInfo))
-    print("Ассиметричный открытый ключ записан в файл")
-    # сериализация закрытого ключа в файл
-    with open(path_close, 'wb') as private_out:
-        private_out.write(private_key.private_bytes(encoding=serialization.Encoding.PEM,
-                                                    format=serialization.PrivateFormat.TraditionalOpenSSL,
-                                                    encryption_algorithm=serialization.NoEncryption()))
-    print("Ассиметричный закрытый ключ записан в файл")
-    # Зашифровать ключ симметричного шифрования открытым ключом и сохранить по указанному пути.
-    with open(path_open, "rb") as f:
-        public_bytes = f.read()
-    d_public_key = load_pem_public_key(public_bytes)
-    print(d_public_key)
-    ciphertext = public_key.encrypt(
-        key,
-        padding.OAEP(
-            mgf=padding.MGF1(algorithm=hashes.SHA256()),
-            algorithm=hashes.SHA256(),
-            label=None
-        )
-    )
-    print("Зашифрование ключа симметричного шифрования открытым ключом")
-    with open(path_enc, "wb") as f:
-        f.write(ciphertext)
-    print("Зашифрованный симметричный ключ записан в файл")
-
-# Входные параметры:
-# 1) путь к шифруемому текстовому файлу (очевидно, что файл должен быть достаточно объемным);
-# 2) путь к закрытому ключу ассиметричного алгоритма;
-# 3) путь к зашированному ключу симметричного алгоритма;
-# 4) путь, по которому сохранить зашифрованный текстовый файл;
-# 2.1. Расшифровать симметричный ключ.
-# 2.2. Зашифровать текст симметричным алгоритмом и сохранить по указанному пути.
 
 def decryption_sem_key():
     """Расшифровать симметричный ключ"""
     print("Расшифровываем симметричный ключ")
-    path_enc = input("Введите путь к файлу с зашифрованным симметричным ключом ")
+    path_enc = input(
+        "Введите путь к файлу с зашифрованным симметричным ключом ")
     if os.path.getsize(path_enc) == 0:
         raise "Файл не существует"
     with open(path_enc, mode="rb") as f:
@@ -140,8 +90,6 @@ def decryption_sem_key():
     return dc_key
 
 
-
-
 def encryption_text_sym_alg(sym_key):
     """Зашифровать текст симметричным алгоритмом и сохранить по указанному пути"""
     print("Зашифруем текст симметричным алгоритмом и сохраним по указанному пути")
@@ -153,7 +101,7 @@ def encryption_text_sym_alg(sym_key):
         key_file.write(iv)
     padder = padding2.ANSIX923(64).padder()
     padded_text = padder.update(bytes(text, 'utf-8')) + padder.finalize()
-    cipher = Cipher(algorithms.IDEA(sym_key), mode = modes.CBC(iv))
+    cipher = Cipher(algorithms.IDEA(sym_key), mode=modes.CBC(iv))
     encryptor = cipher.encryptor()
     en_text = encryptor.update(padded_text) + encryptor.finalize()
     print("Текст зашифрован!")
@@ -161,49 +109,15 @@ def encryption_text_sym_alg(sym_key):
     with open(path_save, 'wb') as f:
         f.write(en_text)
 
+
 def encryption_data_without():
     try:
         decrypt_key = decryption_sem_key()
         encryption_text_sym_alg(decrypt_key)
-    except:
+    except BaseException:
         print("Error")
 
-def encryption_data(path_file, path_close, path_enc, path_save):
 
-    # 2.1. Расшифровать симметричный ключ.
-    with open(path_enc, mode="rb") as f:
-        en_key = f.read()
-
-    with open(path_close, 'rb') as pem_in:
-        private_key = pem_in.read()
-    dc_private_key = load_pem_private_key(private_key, password=None, )
-    dc_key = dc_private_key.decrypt(en_key,
-                                   padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
-                                                label=None))
-    print("Дешифрованный ключ: ", dc_key)
-    # 2.2. Зашифровать текст симметричным алгоритмом и сохранить по указанному пути.
-    with open(path_file, 'r', encoding='utf-8') as f:
-        text = f.read()
-
-    iv = os.urandom(8)
-    with open("iv.bin", 'wb') as key_file:
-        key_file.write(iv)
-    padder = padding2.ANSIX923(64).padder()
-    padded_text = padder.update(bytes(text, 'utf-8')) + padder.finalize()
-    cipher = Cipher(algorithms.IDEA(dc_key), mode = modes.CBC(iv))
-    encryptor = cipher.encryptor()
-    en_text = encryptor.update(padded_text) + encryptor.finalize()
-    print("Текст зашифрован!")
-    with open(path_save, 'wb') as f:
-        f.write(en_text)
-
-# Входные парметры:
-# 1) путь к зашифрованному текстовому файлу;
-# 2) путь к закрытому ключу ассиметричного алгоритма;
-# 3) путь к зашированному ключу симметричного алгоритма;
-# 4) путь, по которому сохранить расшифрованный текстовый файл.
-# 3.1. Расшифровать симметричный ключ.
-# 3.2. Расшифровать текст симметричным алгоритмом и сохранить по указанному пути.
 def decrypt_text_sym_alg(dc_key):
     """Расшифровать текст симметричным алгоритмом и сохранить по указанному пути"""
     print("Расшифровать текст симметричным алгоритмом и сохранить по указанному пути")
@@ -230,45 +144,16 @@ def decryption_data_without():
     try:
         dc_key = decryption_sem_key()
         decrypt_text_sym_alg(dc_key)
-    except:
+    except BaseException:
         print("Error")
-
-
-def decryption_data(path_save, path_close, path_enc, path_decr):
-    # 3.1. Расшифровать симметричный ключ.
-    with open(path_enc, mode="rb") as f:
-        en_key = f.read()
-
-    with open(path_close, 'rb') as pem_in:
-        private_key = pem_in.read()
-    dc_private_key = load_pem_private_key(private_key, password=None, )
-    dc_key = dc_private_key.decrypt(en_key,
-                                    padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()), algorithm=hashes.SHA256(),
-                                                 label=None))
-    print("Дешифрованный ключ: ", dc_key)
-    # 3.2. Расшифровать текст симметричным алгоритмом и сохранить по указанному пути.
-    with open(path_save, 'rb') as f:
-        en_text = f.read()
-
-    with open("iv.bin", "rb") as f:
-        iv = f.read()
-    cipher = Cipher(algorithms.IDEA(dc_key), modes.CBC(iv))
-    decryptor = cipher.decryptor()
-    dc_text = decryptor.update(en_text) + decryptor.finalize()
-    unpadder = padding2.ANSIX923(32).unpadder()
-    unpadded_dc_text = unpadder.update(dc_text) + unpadder.finalize()
-    print(unpadded_dc_text.decode("UTF-8"))
-    print("Текст расшифрован!")
-
-    with open(path_decr, 'w') as f:
-        f.write(dc_text.decode("UTF_8"))
-    print("Текст Записан в файл")
 
 
 if __name__ == '__main__':
     check = True
     while check:
-        choice = int(input('1. Генерация ключей гибридной системы\n2. Шифрование данных гибридной системой\n3. Дешифрование данных гибридной системой\n4.Выход\n'))
+        choice = int(input(
+            '1. Генерация ключей гибридной системы\n2. Шифрование данных гибридной системой\n3.'
+            ' Дешифрование данных гибридной системой\n4.Выход\n'))
         if choice == 1:
             hybrid_system_key_generate_without()
         if choice == 2:
@@ -278,7 +163,3 @@ if __name__ == '__main__':
         if choice == 4:
             check = False
             break
-
-
-
-
